c26e7617f0ea41355bae2260959f756d59b8cfae
Sajankjose <sajankjose@gmail.com>
2026-08-09 12:17:57 +0530

    Update TopNavBar.tsx

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useAuth } from "../auth/useAuth";
 import { useNavigate } from "react-router";
 import { Bell, Settings, User, LogOut, ChevronDown } from "lucide-react";
 
