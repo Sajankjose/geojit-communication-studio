97ab1ce6791ea7a2b0f7d48d8bc90fd0f2d8ac48
Sajankjose <sajankjose@gmail.com>
2026-08-27 15:34:47 +0530

    Create index.ts

---
+++ src/app/design-system/index.ts
@@ -0,0 +1,34 @@
+export {
+  designSystemContracts,
+} from "./contracts";
+
+export type {
+  IconSemanticRole,
+} from "./contracts";
+
+export {
+  DesignSystemButton,
+} from "./DesignSystemButton";
+
+export type {
+  DesignSystemButtonProps,
+  DesignSystemButtonSize,
+  DesignSystemButtonVariant,
+} from "./DesignSystemButton";
+
+export {
+  DesignSystemCard,
+} from "./DesignSystemCard";
+
+export type {
+  DesignSystemCardProps,
+  DesignSystemCardSurface,
+} from "./DesignSystemCard";
+
+export {
+  DesignSystemIcon,
+} from "./DesignSystemIcon";
+
+export type {
+  DesignSystemIconProps,
+} from "./DesignSystemIcon";
